5b3a96c0efd761296fea70f3a910dc3a63496993
akshatasangale16 <sangaleshubhangi308@gmail.com>
2025-03-20 06:27:56 +0530

    Added Hello World Java application

---
+++ Hellow.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello My Name is Akshata.");
+        System.out.println("This is my Experiment No 4");
+    }
+}
